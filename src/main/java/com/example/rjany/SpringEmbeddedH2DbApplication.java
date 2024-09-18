b0c89dc66996a234191486306dba95bd3ba528a9
Animesh Naskar <Animesh.Naskar+EYGS@gds.ey.com>
2024-09-18 12:39:04 +0530

    Pom modified

--- src/main/java/com/example/rjany/SpringEmbeddedH2DbApplication.java
+++ src/main/java/com/example/rjany/SpringEmbeddedH2DbApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringEmbeddedH2DbApplication {
 
-	//test
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringEmbeddedH2DbApplication.class, args);
 	}
